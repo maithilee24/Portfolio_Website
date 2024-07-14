c66e1f762f9bd2796855ec02b0aa68284075a4fa
maithilee <pardhia.ug22.ec@nitp.ac.in>
2024-07-15 02:14:49 +0530

    Responsive hero,navbar section

--- Frontend/my-app/src/Components/contact/contact.jsx
+++ Frontend/my-app/src/Components/contact/contact.jsx
@@ -10,13 +10,13 @@ import Linkedln from "../images/Linkedln.png";
 
 function Contact(){
     return (
-        <div className="flex flex-col mt-8 bg-gradient-to-r from-[#4A39C3] to-[#509AD7] h-[100vh] rounded-t-[12rem]">
+        <div className="flex flex-col min-h-screen mt-8 bg-gradient-to-r from-[#4A39C3] to-[#509AD7]  rounded-t-[12rem]">
             <div className="flex justify-center items-center mt-10">
                 <div><img src={Star} alt=""></img></div>
                 <div className="text-white text-3xl mx-2 font-medium">CONATCT</div>
                 <div><img src={Star} alt=""></img></div>
             </div>
-            <div className="flex flex-row mx-40 mt-20">
+            <div className="flex flex-row mx-40 mt-20 ">
                 <div className="flex flex-col w-1/2">
                         <div className="text-white text-3xl font-medium mb-3">DON'T BE SHY!</div>
                         <div className="text-white text-[0.95rem] font-light italic mr-16 mb-3">"Let's make something great together! Contact me for collaborations, freelance opportunities, or any inquiries about my work. I'm looking forward to hearing from you."</div>
